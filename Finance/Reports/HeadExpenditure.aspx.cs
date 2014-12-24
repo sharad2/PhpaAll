41a0cdf4ea1b1df0ce832b576fc9dfe379e8fe01
Hemant Varshney <hvarshney@eclsys.com>
2014-12-24 10:12:12 +0530

    Added some more heads in HeadExpenditure and added text in PaybillRegister.aspx

--- Finance/Reports/HeadExpenditure.aspx.cs
+++ Finance/Reports/HeadExpenditure.aspx.cs
@@ -63,7 +63,31 @@ namespace Finance.Reports
             gvHeadsExpTillDate.DataSource = from vd in db.RoVoucherDetails
                                      where (vd.RoHeadHierarchy.HeadOfAccountType == "Expenditure" ||
                                              vd.RoHeadHierarchy.HeadOfAccountType == "TOUR_EXPENSES" ||
-                                             vd.RoHeadHierarchy.HeadOfAccountId == 1372 || vd.RoHeadHierarchy.HeadOfAccountId == 1380 || vd.RoHeadHierarchy.HeadOfAccountId == 1384 || vd.RoHeadHierarchy.HeadOfAccountId == 1385 || vd.RoHeadHierarchy.HeadOfAccountId == 1386 || vd.RoHeadHierarchy.HeadOfAccountId == 1388 || vd.RoHeadHierarchy.HeadOfAccountId == 1389 || vd.RoHeadHierarchy.HeadOfAccountId == 1395 || vd.RoHeadHierarchy.HeadOfAccountId == 1534 || vd.RoHeadHierarchy.HeadOfAccountId == 1397 || vd.RoHeadHierarchy.HeadOfAccountId == 1769) &&
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1373 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1374 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1375 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1376 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1377 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1379 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1381 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1382 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1385 || 
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1395 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1397 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1534 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1593 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1594 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1595 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1596 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1597 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1686 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1687 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1688 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1689 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1690 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1691 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1769 ||
+                                             vd.RoHeadHierarchy.HeadOfAccountId == 1846) &&
                                              vd.RoVoucher.VoucherDate >= dt.Value.FinancialYearStartDate() &&
                                              vd.RoVoucher.VoucherDate <= tbDate.ValueAsDate
                                      group vd by vd.RoHeadHierarchy.TopParentName into grouping
