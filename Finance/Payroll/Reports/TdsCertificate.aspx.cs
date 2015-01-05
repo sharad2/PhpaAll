9ba93eaf966a8a6dbf8bfa518061b28122e517c2
Manmohan Bisht <mbisht@eclsys.com>
2015-01-05 16:24:24 +0530

    Bug Fixed :- In a same month if two salaries are craeted for an employee then TDS certifacate was not showing all the figures correctly

--- Finance/Payroll/Reports/TdsCertificate.aspx.cs
+++ Finance/Payroll/Reports/TdsCertificate.aspx.cs
@@ -46,7 +46,7 @@ namespace Finance.Payroll.Reports
                                 pea.Adjustment.AdjustmentCategoryId : 0,
                            IsDeduction = pea.Adjustment.IsDeduction
                        } into g
-                       orderby g.Key.EmployeePeriod.SalaryPeriod.SalaryPeriodStart, g.Key.IsDeduction, g.Key.CategoryId
+                       orderby g.Key.EmployeePeriod.SalaryPeriod.SalaryPeriodStart,g.Key.EmployeePeriod.SalaryPeriod.SalaryPeriodCode, g.Key.IsDeduction, g.Key.CategoryId
                        let catdesc = g.Where(p => p.Adjustment.AdjustmentCategoryId == g.Key.CategoryId)
                             .Select(p => p.Adjustment.AdjustmentCategory.ShortDescription).First()
                        select new
