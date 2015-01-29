fbc9e4f2b1dafef2d47df509d2bb7fc39724b553
ssinghal <ssinghal@eclsys.com>
2015-01-29 18:03:52 +0530

    Changed jquery path from /Scripts/jquery.min.js to ~/Scripts/jquery.min.js

--- Finance/MasterPage.master.designer.cs
+++ Finance/MasterPage.master.designer.cs
@@ -12,6 +12,15 @@
 public partial class MasterPage {
     
     /// <summary>
+    /// jqueryscripts control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.PlaceHolder jqueryscripts;
+    
+    /// <summary>
     /// JQueryScriptMgr control.
     /// </summary>
     /// <remarks>
