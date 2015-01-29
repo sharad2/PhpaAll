15267de06d9a8d098633cc980ed8fc5f4086b6e6
ssinghal <ssinghal@eclsys.com>
2015-01-29 14:11:27 +0530

    Added files needed for MVC

---
+++ Finance/Controllers/BillsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhpaAll.Controllers
+{
+    public partial class BillsController : Controller
+    {
+        // GET: Bills
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
