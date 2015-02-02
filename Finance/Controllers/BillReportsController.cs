c70a631f371d094f9fdad1f04b06f81e9c49d3e5
ssinghal <ssinghal@eclsys.com>
2015-02-02 18:12:48 +0530

    Reviewing

---
+++ Finance/Controllers/BillReportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhpaAll.Controllers
+{
+    public partial class BillReportsController : Controller
+    {
+        // GET: BillReports
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
