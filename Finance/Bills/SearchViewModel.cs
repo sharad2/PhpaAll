7a19169154310f77b7549394a83f69dd21b0b5fe
deepak bhatt <deepakbhatt12@gmail.com>
2015-02-06 15:17:57 +0530

    working on search

---
+++ Finance/Bills/SearchViewModel.cs
@@ -0,0 +1,25 @@
+﻿//using PhpaBills.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhpaAll.Bills
+{
+    public class SearchModel
+    {
+        //For internal use to retrive row for editing
+        public int Id { get; set; }
+
+        public string BillNumber { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? BillDate { get; set; }
+
+    }
+
+    public class SearchViewModel
+    {
+        public IList<SearchModel> Bills { get; set; }
+    }
+
+}
