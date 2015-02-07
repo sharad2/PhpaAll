b8f6b9cb64febfad93d82358383362593e28d67a
Shivani Bist <sbist@eclsys.com>
2015-02-07 16:10:50 +0530

    Search query

--- Finance/Controllers/BillsHomeController.cs
+++ Finance/Controllers/BillsHomeController.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace PhpaAll.Controllers
 {
@@ -59,21 +60,47 @@ namespace PhpaAll.Controllers
                 var query1 = from bill in _db.Value.Bills
                          where bill.BillNumber.ToLower().Contains(token)
                          select bill;
+                // Extra point if bill number starts with a token
+                var query2 = from bill in _db.Value.Bills
+                             where bill.BillNumber.ToLower().StartsWith(token)
+                             select bill;
+                // Extra point if bill number ends with token
+                var query3 = from bill in _db.Value.Bills
+                             where bill.BillNumber.ToLower().EndsWith(token)
+                             select bill;
+
+
                 if (query == null) {
                     query = query1;
                 }
                 else
                 {
-                    query = query.Union(query1);
+                    query = query.Concat(query1);
                 }
+                query = query.Concat(query2).Concat(query3);
             }
 
             // Max 200
-            query = query.OrderByDescending(p => p.BillDate).Take(200);
+            //query = query.OrderByDescending(p => p.BillDate);
+            // Count how many times the bill was selected. If a bill is selected more times, it is more relevant
+            var query44 = from bill in query
+                         group bill by bill into g
+                         let billNumber = g.Key.BillNumber.ToLower()
+                         let exactMatch = tokens.Contains(billNumber) ? 1 : 0
+                          //let startsWith = g.First().BillNumber.ToLower()
+                         orderby exactMatch descending, g.Count() descending
+                         select g.Key;
+
+            var list = new List<SearchBillModel>();
+
+            foreach (var sbm in BillModel.FromQuery<SearchBillModel>(query44).Take(200))
+            {
+                list.Add(sbm);
+            }
 
             SearchViewModel model = new SearchViewModel
             {
-                Bills = BillModel.FromQuery<SearchBillModel>(query).ToList()
+                Bills = list
             };
 
             return View(Views.Search, model);
