49e961f913af3f8726e5c48df4e2046660ecf79c
Hemant Varshney <hvarshney@eclsys.com>
2015-02-10 19:14:03 +0530

    Capturing audit from front end

--- Finance/Bills/PhpaBills.cs
+++ Finance/Bills/PhpaBills.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Linq;
 using System.IO;
 using System.Web;
+using System.Linq;
 
 namespace PhpaAll.Bills
 {
@@ -34,18 +37,6 @@ namespace PhpaAll.Bills
 
         partial void UpdateBill(Bill instance)
         {
-            var info = this.Bills.GetModifiedMembers(instance);
-
-            foreach (var x in info)
-            {
-                var audit = new BillAudit();
-                var y = x.OriginalValue;
-                var y1 = x.CurrentValue;
-                var y2 = x.Member.Name;
-                this.BillAudits.InsertOnSubmit(audit);
-            }
-            throw new NotImplementedException("TODO: Add update audit");
-
             instance.ModifiedBy = HttpContext.Current.User.Identity.Name;
             instance.Modified = DateTime.Now;
             ExecuteDynamicUpdate(instance);
@@ -57,5 +48,51 @@ namespace PhpaAll.Bills
             ExecuteDynamicInsert(instance);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="failureMode"></param>
+        /// <remarks>
+        /// http://stackoverflow.com/questions/9158237/linq-to-sql-audit-trail-find-old-values-in-submitchanges
+        /// </remarks>
+        public override void SubmitChanges(System.Data.Linq.ConflictMode failureMode)
+        {
+            ChangeSet changeSet = this.GetChangeSet();
+
+            // Put the updated objects into a IEnumerable
+            IEnumerable<object> updatedEntities = changeSet.Updates;
+
+            foreach (var bill in updatedEntities.OfType<Bill>())
+            {
+                // For each bill updated
+                var properties = this.GetTable(bill.GetType()).GetModifiedMembers(bill);
+                // Do something with the old values
+                var audit = new BillAudit2();
+
+                audit.BillId = bill.Id;
+                audit.CreatedBy = HttpContext.Current.User.Identity.Name;
+                audit.Created = DateTime.Now;
+
+                this.BillAudit2s.InsertOnSubmit(audit);
+
+                foreach (var prop in properties)
+                {
+                    // For each property updated
+                    var auditDetail = new BillAuditDetail();
+
+                    auditDetail.OldValue = string.Format("{0}", prop.OriginalValue);
+                    auditDetail.NewValue = string.Format("{0}", prop.CurrentValue);
+                    auditDetail.FieldName = prop.Member.Name;
+                    auditDetail.CreatedBy = HttpContext.Current.User.Identity.Name;
+                    auditDetail.Created = DateTime.Now;
+
+                    audit.BillAuditDetails.Add(auditDetail);
+                }
+
+            }
+            base.SubmitChanges(failureMode);
+        }
     }
+    
+
 }
