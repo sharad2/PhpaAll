4eff08d89610ed4ca35fcf5d70a915e33e8b1c32
ssinghal <ssinghal@eclsys.com>
2015-02-16 17:32:46 +0530

    Created partial javascript files

--- Finance/T4MVC.cs
+++ Finance/T4MVC.cs
@@ -103,10 +103,11 @@ namespace Links
             private const string URLPATH = "~/Views/ManageBills";
             public static string Url() { return T4MVCHelpers.ProcessVirtualPath(URLPATH); }
             public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(URLPATH + "/" + fileName); }
-            public static readonly string Bill_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/Bill.min.css") ? Url("Bill.min.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Bill.min.css") : Url("Bill.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Bill.css");
-                 
             public static readonly string Bill_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/Bill.min.js") ? Url("Bill.min.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Bill.min.js") : Url("Bill.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Bill.js");
-                    }
+                    public static readonly string Create_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/Create.min.js") ? Url("Create.min.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Create.min.js") : Url("Create.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/Create.js");
+                    public static readonly string dropzone_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/dropzone.min.css") ? Url("dropzone.min.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/dropzone.min.css") : Url("dropzone.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/dropzone.css");
+                 
+        }
     
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
         public static class Shared {
@@ -117,9 +118,6 @@ namespace Links
                     public static readonly string layout_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/layout.min.css") ? Url("layout.min.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.min.css") : Url("layout.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.css");
                  
             public static readonly string layout_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/layout.min.js") ? Url("layout.min.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.min.js") : Url("layout.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.js");
-                    public static readonly string layout_partial_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/layout.partial.min.css") ? Url("layout.partial.min.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.partial.min.css") : Url("layout.partial.css")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.partial.css");
-                 
-            public static readonly string layout_partial_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/layout.partial.min.js") ? Url("layout.partial.min.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.partial.min.js") : Url("layout.partial.js")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/layout.partial.js");
                     }
     
         public static readonly string web_config = Url("web.config")+"?"+T4MVCHelpers.TimestampString(URLPATH + "/web.config");
---
+++ Finance/Views/ManageBills/Create.js
@@ -0,0 +1,48 @@
+﻿///#source 1 1 Create.partial.js
+
+// Disable auto discover for all elements:
+//Dropzone.autoDiscover = false;
+
+$(document).ready(function () {
+	"use strict";
+	//var myDropzone = new Dropzone("div#mydropzone", { url: "/file/post", autoProcessQueue: false });
+
+	$('#tbDivision,#tbContractor').each(function (index, elem) {
+		$(elem).typeahead({
+			highlight: true
+		}, {
+			//name: 'Division',
+			displayKey: 'label',
+			source: function (query, cb) {
+				var url = $(elem).attr('data-url').replace('~', query);
+				$.get(url).done(function (data, textStatus, jqXHR) {
+					this.cb(data);
+				}.bind({ cb: cb })).fail(function (jqXHR, textStatus, errorThrown) {
+					if (jqXHR.status == 500) {
+						this.cb([{ label: 'Error ' + (jqXHR.responseText || errorThrown), value: '' }]);
+					} else {
+						this.cb([{ label: 'Http Error ' + jqXHR.status + ': ' + errorThrown + ' ' + this.url, value: '' }]);
+					}
+				}.bind({ cb: cb, url: url }));
+			},
+			templates: {
+				empty: 'No matching results found'
+			}
+		}).on('typeahead:selected typeahead:autocompleted', function (e, sug, ds) {
+			// Store the id of the selected division in the hdden field
+			//$('#hfDivisionId').val(sug.value);
+			$(this).closest('div.form-group').find('input:not(".typeahead")').val(sug.value);
+		}).on('input', function (e) {
+			// When user changes the divisionId, empty the hidden field
+			$(this).closest('div.form-group').find('input:not(".typeahead")').val('');
+		});
+
+	});
+
+
+	$("#billDatePicker,#divSubDatePicker,#dueDatePicker").datetimepicker({
+		format: 'D/M/YYYY'
+	});
+
+});
+
